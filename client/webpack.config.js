51a01d370ada311c63cc993ba39d99cdb990bb0f
yahm0 <stnt190@outlook.com>
2024-05-28 10:24:18 -0400

    added logic to the webpack.config.js file. Imports only so far.

---
+++ client/webpack.config.js
@@ -0,0 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
+const path = require('path');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
