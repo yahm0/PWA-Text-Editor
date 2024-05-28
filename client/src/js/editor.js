974b5b2f45b8e8707c6c3e1edb07a66cce5133c2
yahm0 <stnt190@outlook.com>
2024-05-28 10:43:22 -0400

    added logic to multiple files.

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,34 +1,30 @@
 import { getDb, putDb } from './idb';
+import CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/javascript/javascript';
 
-const editor = document.getElementById('editor');
+const editorContainer = document.getElementById('editor');
 
-window.addEventListener('DOMContentLoaded', async () => {
-  const localData = await getDb();
-  const remoteData = await fetch('/api/notes')
-    .then(response => response.json())
-    .then(data => data.length ? data[0].content : null)
-    .catch(error => console.error('Error fetching remote data:', error));
-
-  editor.innerHTML = remoteData || localData || 'Start typing...';
+const editor = CodeMirror(editorContainer, {
+  mode: 'javascript',
+  lineNumbers: true,
+  theme: 'material-darker',
+  value: '/*\n  J.A.T.E\n  just another text editor\n*/\nconsole.log("Hello world!")\n'
 });
 
-editor.addEventListener('blur', () => {
-  const content = editor.innerHTML;
-  putDb(content);
+window.addEventListener('DOMContentLoaded', async () => {
+  const data = await getDb();
+  editor.setValue(data || editor.getValue());
+});
 
-  // Save note to the server
-  fetch('/api/notes', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ content }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Note saved:', data);
-    })
-    .catch(error => {
-      console.error('Error saving note:', error);
-    });
+editor.on('blur', () => {
+  putDb(editor.getValue());
+});
+
+document.getElementById('installBtn').addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+  if (promptEvent) {
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+  }
 });
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,58 +4,58 @@ const path = require('path');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
-    entry: './src/js/main.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.js',
-    },
-    mode: 'development',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
         },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+    new WebpackPwaManifest({
+      name: 'J.A.T.E - Just Another Text Editor',
+      short_name: 'J.A.T.E',
+      description: 'A simple text editor as a PWA',
+      background_color: '#ffffff',
+      theme_color: '#007bff',
+      start_url: '/',
+      publicPath: '/',
+      icons: [
         {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('icons'),
         },
       ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-      }),
-      new GenerateSW({
-        clientsClaim: true,
-        skipWaiting: true,
-      }),
-      new WebpackPwaManifest({
-        name: 'PWA Text Editor',
-        short_name: 'TextEditor',
-        description: 'A simple text editor as a PWA',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        start_url: '/',
-        publicPath: '/',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('icons'),
-          },
-        ],
-      }),
-    ],
-  };
+    }),
+  ],
+};
